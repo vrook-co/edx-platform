e2711e9a0a4c13a1fce9241c44dd3eee3b90d808
varunramesh26 <varunramesh.07@gmail.com>
2020-09-09 13:49:55 +0000

    added the registration form fields successfully, but not able to save it in database!

---
+++ lms/djangoapps/custom_reg_form/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class CustomRegFormConfig(AppConfig):
+    name = 'custom_reg_form'
---
+++ lms/djangoapps/custom_reg_form/forms.py
@@ -0,0 +1,40 @@
+from .models import ExtraInfo
+from django.forms import ModelForm
+
+class ExtraInfoForm(ModelForm):
+    """
+    The fields on this form are derived from the ExtraInfo model in models.py.
+    """
+    def __init__(self, *args, **kwargs):
+        super(ExtraInfoForm, self).__init__(*args, **kwargs)
+
+        self.fields['branch'].error_messages = {
+            "required": u"Please enter your branch.",
+            "invalid": u"Sorry, you may have entered a non-existent branch.",
+        }
+
+        self.fields['enrollment_year'].error_messages = {
+            "required": u"Enter your year of joining.",
+            "invalid": u"Sorry, you may have entered an invalid year.",
+        }
+
+        self.fields['preferred_language'].error_messages = {
+            "required": u"Your preferred medium of language.",
+            "invalid": u"Sorry, you may have entered a non-existent language",
+        }
+
+        self.fields['preferred_multimedia'].error_messages = {
+            "required": u"Your preferred medium of learning.",
+            "invalid": u"Sorry, you may have entered a non-existent multimedia.",
+        }
+
+        self.fields['interests'].error_messages = {
+            "required": u"Please enter your areas of interests.",
+            "invalid": u"Interests not available",
+        }
+
+
+    class Meta(object):
+        model = ExtraInfo
+        fields = ('branch', 'enrollment_year', 'preferred_language', 'preferred_multimedia', 'interests')
+
